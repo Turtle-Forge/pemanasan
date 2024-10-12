e348b92aa77dde5697f4347be37adddb8be9e245
Benony Gabriel <gbenn.work@gmail.com>
2024-10-13 02:08:24 +0700

    belajar pengkondisian

---
+++ Basics/02_Conditions/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int in = -9;
+
+    if (in % 2 == 0)
+    {
+        cout << in << " adalah bilangan GENAP" << endl;
+    }
+    else
+    {
+        cout << in << " adalah bilangan GANJIL" << endl;
+    }
+
+    return 0;
+}
