91d47b634fcdb31f83a7848f7860949e0d2647ef
Benony Gabriel <gbenn.work@gmail.com>
2025-02-21 01:36:31 +0700

    Add Java03_TipeDataString class to demonstrate string data type usage

---
+++ OOP/01-Java Basics/src/Java03_TipeDataString.java
@@ -0,0 +1,15 @@
+public class Java03_TipeDataString {
+    public static void main(String[] args) {
+        // Tipe data string
+        // Tipe data string digunakan untuk menyimpan teks.
+        // Tipe data string adalah tipe data yang berbeda dari tipe data number.
+        // Tipe data string adalah tipe data yang digunakan untuk menyimpan data berupa
+        // teks
+        String text = "Hello, World!";
+        System.out.println(text);
+
+        text = "Saya belajar Java.";
+        System.out.println(text);
+    }
+
+}
