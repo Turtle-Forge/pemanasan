d3a96ba22e8d615ab2eb6305a5c3c044fc3c770b
Benony Gabriel <gbenn.work@gmail.com>
2025-02-21 01:20:25 +0700

    Add Java Basics README and initial Main class implementation

---
+++ OOP/01-Java Basics/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Java Output
+        // Print text to the console with new line
+        System.out.println("Hello, World!");
+        System.out.println("Saya belajar Java.");
+        System.out.println("Belajar Java sangat menyenangkan!\n");
+
+        // Print text to the console without new line
+        System.out.print("Java adalah bahasa pemrograman yang powerful! ");
+        System.out.print("Java mudah untuk dipelajari.\n\n");
+
+        // Print number
+        System.out.println(50);
+        System.out.println(3.14);
+        System.out.println(1000);
+        System.out.println(10 * 5);
+        System.out.println("10 * 3 = " + (10 * 3));
+        System.out.println("10 / 3 = " + (10 / 3));
+        System.out.println("10.0 / 3.0 = " + (10.0 / 3.0));
+        System.out.println("10 % 3 = " + (10 % 3));
+        System.out.println("10 % 2 = " + (10 % 2));
+
+        // Print boolean
+        System.out.println(true); // output: true
+        System.out.println(false);
+
+        // Java comments
+        // This is a single line comment
+        /*
+         * ini adalah komentar multi line
+         * ini adalah komentar multi line
+         * ini adalah komentar multi line
+         * ini adalah komentar multi line
+         */
+
+    }
+}
