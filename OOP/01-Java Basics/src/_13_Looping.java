3c36e9f6fd2d627690808438958d48967c1d0277
Benony Gabriel <gbenn.work@gmail.com>
2025-02-26 06:09:58 +0700

    Add demonstration of for, while, and do-while loops in _13_Looping class

---
+++ OOP/01-Java Basics/src/_13_Looping.java
@@ -0,0 +1,22 @@
+public class _13_Looping {
+    public static void main(String[] args) {
+        // For Loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Perulangan ke-" + i);
+        }
+
+        // While Loop
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }
+
+        // Do While Loop
+        int j = 0;
+        do {
+            System.out.println("Perulangan ke-" + j);
+            j++;
+        } while (j < 5);
+    }
+}
